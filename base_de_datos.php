64fd9e9728d1aa0bbf58bce9408b8749d75e2a96
odenfis <wmoncada@tecsup.edu.pe>
2023-04-17 20:12:15 -0500

    version2

--- base_de_datos.php
+++ base_de_datos.php
@@ -3,12 +3,12 @@
 Credenciales para la conexión DB
 ================================
  */
-$contraseña = "89385303dccb322c56224180e01180e3ed3a4cdab342b192e5ae41033ea44e85";
-$usuario = "taylhpggsxxkff";
-$nombreBaseDeDatos = "d9v66eor6b4q7e";
+$contraseña = "ulQJFGUyYDAcMMyDXBbV";
+$usuario = "postgres";
+$nombreBaseDeDatos = "railway";
 # Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
-$rutaServidor = "ec2-44-207-133-100.compute-1.amazonaws.com";
-$puerto = "5432";
+$rutaServidor = "containers-us-west-41.railway.app";
+$puerto = "6512";
 try {
     $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
