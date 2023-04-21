8c7d635425ce7b2d80dde831bc2fd168a5295c2e
Rasecmoreno <mekoxplay@gmail.com>
2023-04-21 11:01:46 -0500

    practica

--- encabezado.php
+++ encabezado.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Conexión de PHP con PostgreSQL usando PDO">
     <meta name="author" content="Odenfis">
-    <title>PostgreSQL-PHP | Por Walter Moncada</title>
+    <title>PostgreSQL-PHP | Por Cesar Sanchez</title>
     <!-- Cargar el CSS de Boostrap-->
     <link href="./bootstrap.min.css" rel="stylesheet">
     <!-- Cargar estilos propios -->
